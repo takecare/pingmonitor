d05427bcaf12367bfbfc4a6a4d6b9697ee6c1e81
Rui Teixeira <rui@vazteixeira.org>
2019-03-16 09:53:43 +0000

    refactor function() to lambda

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
 const fs = require('fs')
 
-function ingest(path) {
-    return new Promise((resolve, reject) => {
-        fs.readFile(path, 'utf8',  (err, data) => {
-            if (err) {
-                reject(err)
-            } else {
-                resolve(data)
-            }
-        })
+const ingest = path => new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+        if (err) {
+            reject(err)
+        } else {
+            resolve(data)
+        }
     })
-}
+})
 
 const count = (requests, request) => {
     if (request.includes('timeout')) {
@@ -23,13 +21,12 @@ const count = (requests, request) => {
 
 const file = () => process.argv[2] ? process.argv[2] : 'results.txt'
 const contents = ingest(file())
-  .then(contents => contents.split('\n'))
+    .then(contents => contents.split('\n'))
 
 const timestampRegex = /\d{2}:\d{2}:\d{2}/
 
 contents
-  // .then(lines => lines.filter(line => timestampRegex.test(line)))
-  .then(lines => lines.filter(line => line.includes('icmp_seq')))
-  .then(requests => requests.reduce(count, { timeout: 0, success: 0, total: requests.length }))
-  .then(result => ({ failure: (result.timeout / result.total), success: (result.success / result.total) }))
-  .then(console.log)
+    .then(lines => lines.filter(line => line.includes('icmp_seq')))
+    .then(requests => requests.reduce(count, { timeout: 0, success: 0, total: requests.length }))
+    .then(result => ({ failure: (result.timeout / result.total), success: (result.success / result.total) }))
+    .then(console.log)
